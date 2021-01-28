5253d0158714fdf95a30996a432b440de1c5ad8c
mborycki <borqs11@gmail.com>
2021-01-28 12:33:44 +0100

    Hangman game has been finished

--- hangman.py
+++ hangman.py
@@ -4,15 +4,37 @@ import string
 
 def choose_a_word(hangman_words):
     word = random.choice(hangman_words)
-    while '-' in hangman_words or ' ' in hangman_words: # we do not need spaces or dashes in our word
+    while '-' in word or ' ' in word: # we do not need spaces or dashes in our word
         word = random.choice(hangman_words)
     return word.upper()
 
-word = choose_a_word(hangman_words)
-word_letters = set(word)
-
-alphabet = set(string.ascii_uppercase)
-
-print(word)
-print(word_letters)
-print(alphabet)
+def hangman_game(lives_number):
+    '''
+    Argument: lives = amount of lives you wanna have
+    '''
+    underline = ("\n-----------------------\n")
+    lives = lives_number
+    alphabet = set(string.ascii_uppercase)
+    chosen_word = choose_a_word(hangman_words)
+    word_letters = set(chosen_word)
+    used_letters = set()
+    print(f"Word: {chosen_word}\n")
+    
+    while len(word_letters) > 0 and lives >= 1:
+        guessed_letters = [letter if letter in used_letters else "_" for letter in chosen_word]
+        print(f"Remaining life: {lives}")
+        print("\nCurrent word: "," ".join(guessed_letters))
+        user_letter = input("\nWrite a letter: ").upper()
+        
+        if user_letter in word_letters and user_letter in alphabet:
+            print(f"{underline}You guessed the letter: '{user_letter}'{underline}")
+            word_letters.remove(user_letter)
+            used_letters.add(user_letter)
+        else:
+            print(f"{underline}Sorry try to guess another letter{underline}")
+            lives -= 1
+    
+    if lives == 0:
+        print(f"\n{underline}Sorry but you're dead. The chosen word was: '{chosen_word}'{underline}")
+    else:
+        print(f"\n{underline}Congratulation! You guessed the chosen word was: '{chosen_word}'{underline}")
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import guessing_game as gg
 import RockScissorsPaper as rsp
+import hangman
 
 print('Hello. Please choose a game:\n'
 'Guessing Game for User: (A)\n'
 'Guessing Game for Computer: (B)\n'
-'Rock Scissors Paper: (C)')
-chosen_game = input('Please type A \ B \ C: ').upper()
+'Rock Scissors Paper: (C)\n'
+'Hangman (D)')
+chosen_game = input('Please type A \ B \ C \ D: ').upper()
 
 if chosen_game == 'A':
     gg.user_guess(10)
@@ -14,3 +16,5 @@ elif chosen_game == 'B':
     gg.pc_guess(1,10,name)
 elif chosen_game == 'C':
     rsp.rock_scissors_paper()
+elif chosen_game == 'D':
+    hangman.hangman_game(5)
